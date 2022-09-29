72c204e43a63f6bf21ea898cb7fe7337d63fd6e4
aliahsan676 <aliahsan676@gmail.com>
2022-09-29 18:50:39 +0600

    new while loop

---
+++ src/test/java/whileLoop.java
@@ -0,0 +1,11 @@
+package test.java;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 8){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
