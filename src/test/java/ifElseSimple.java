04f979439466c97beafcabd25ce2c051715f3655
aliahsan676 <aliahsan676@gmail.com>
2022-09-29 18:19:06 +0600

    first commit

---
+++ src/test/java/ifElseSimple.java
@@ -0,0 +1,13 @@
+package test.java;
+
+public class ifElseSimple {
+    public static void main(String[] args) {
+
+        int mark = 80;
+        if (mark >= 80) {
+            System.out.println("Grade = A+");
+        } else {
+            System.out.println("Grade = A");
+        }
+    }
+}
---
+++ src/test/java/ifSimple.java
@@ -0,0 +1,11 @@
+package test.java;
+
+public class ifSimple {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 15;
+        if (x > y) {
+            System.out.println("Greater Number is :" + x);
+        }
+    }
+}
