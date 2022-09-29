e36621a9833deb15baa45ee3587f77827b9e895d
aliahsan676 <aliahsan676@gmail.com>
2022-09-29 18:43:25 +0600

    adding for loop

---
+++ src/test/java/forLoop.java
@@ -0,0 +1,10 @@
+package test.java;
+
+public class forLoop {
+        public static void main(String[] args) {
+            for (int i = 0; i < 7; i++) {
+                System.out.println(i);
+            }
+        }
+
+}
